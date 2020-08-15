9fa8546d0fa2cd5d8232f2aefbe1960746c7e702
PocketMars <51007356+PocketMars@users.noreply.github.com>
2020-08-15 16:03:24 -0500

    ok

---
+++ include/Furnace.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Hotdog.hpp"
+#include "Sprite.hpp"
+#include "Animation.hpp"
+
+class Furnace : public Sprite
+{
+public:
+	Furnace();
+	Furnace(const SpriteCreateInfo& p_info);
+
+	//void update(const float& p_dt) override;
+
+	//void onCollisionBegin(Sprite* p_sprite) override;
+	//void duringCollision(Sprite* p_sprite) override;
+
+	//void animate(const float& p_dt);
+private:
+	//std::map<std::string, AnimationCycle> animationSet;
+};
---
+++ src/furnace.cpp
@@ -0,0 +1,23 @@
+#include "Furnace.hpp"
+#include <string>
+#include <iostream>
+
+Furnace::Furnace()
+{
+
+}
+Furnace::Furnace(const SpriteCreateInfo& p_info)
+:Sprite(p_info),
+{
+
+}
+
+/*void Furnace::update(const float& p_dt)
+{
+	animate(p_dt);
+}
+
+void Furnace::animate(const float& p_dt)
+{
+	
+}*/
