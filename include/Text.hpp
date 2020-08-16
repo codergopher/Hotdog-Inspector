ac1e3919a3721d35e94c96b114c7a513a60e33b3
codergopher <57048930+codergopher@users.noreply.github.com>
2020-08-16 15:51:21 -0600

    haha

--- include/Text.hpp
+++ include/Text.hpp
@@ -8,10 +8,10 @@ class Text : public Sprite
 {
 public:
 	Text();
-	Text(const SpriteCreateInfo& p_info, const char* p_string, TTF_Font* p_font, SDL_Color* p_color);
+	Text(const SpriteCreateInfo& p_info, const char* p_string, TTF_Font* p_font, SDL_Color& p_color);
 
 	void update(const float& p_dt) override;
-	void setText(const char* p_string);
+	void setText(const char* p_string, TTF_Font* p_font, SDL_Color& p_color);
 
 private:
 	
--- src/text.cpp
+++ src/text.cpp
@@ -4,17 +4,17 @@ Text::Text()
 {
 
 }
-Text::Text(const SpriteCreateInfo& p_info, const char* p_string, TTF_Font* p_font, SDL_Color* p_color)
+Text::Text(const SpriteCreateInfo& p_info, const char* p_string, TTF_Font* p_font, SDL_Color& p_color)
 :Sprite(p_info)
 {
-	SDL_Surface* surfaceMessage = TTF_RenderText_Blended(p_font, p_string, p_olor);
+	SDL_Surface* surfaceMessage = TTF_RenderText_Blended(p_font, p_string, p_color);
 	tex = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
 	frameSize = Vector2i(surfaceMessage->w, surfaceMessage->h);
 }
 
-void Text::setText(const char* p_string, TTF_Font* p_font, SDL_Color p_color)
+void Text::setText(const char* p_string, TTF_Font* p_font, SDL_Color& p_color)
 {
-	SDL_Surface* surfaceMessage = TTF_RenderText_Blended(p_font, p_string, p_olor);
+	SDL_Surface* surfaceMessage = TTF_RenderText_Blended(p_font, p_string, p_color);
 	tex = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
 	frameSize = Vector2i(surfaceMessage->w, surfaceMessage->h);
 }
