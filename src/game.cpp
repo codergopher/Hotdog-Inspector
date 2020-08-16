72cdfb544da5368b5dd588d0718d126dd10fd0b5
codergopher <57048930+codergopher@users.noreply.github.com>
2020-08-16 15:23:21 -0600

    haha
    haha

--- src/game.cpp
+++ src/game.cpp
@@ -261,7 +261,7 @@ void Game::loadWorld()
 	}
 
 	 // Score text
-	 /*{
+	 {
 	 	SpriteCreateInfo createInfo = {};
 	 	createInfo.name = "Text";
 	 	createInfo.tex = textures["Font 0"];
@@ -276,7 +276,7 @@ void Game::loadWorld()
 	 	createInfo.zoomModifier = 1.f;
 	 	world.createText(createInfo, "00000", 10);	
 	 	//world.createCharacter(createInfo, "e", 8);
-	 }*/
+	 }
 
 	// UI Lives
 	 {
@@ -330,25 +330,24 @@ void Game::loadWorld()
 	}
 
 	// Menu
-	// {
-	// 	SpriteCreateInfo createInfo = {};
-	// 	createInfo.name = "Menu";
-	// 	createInfo.tex = textures["Menu 0"];
-	// 	createInfo.alpha = 255;
-	// 	createInfo.color = Color(0, 0, 0);
-	// 	createInfo.flip = SDL_FLIP_NONE;
-	// 	createInfo.pos = Vector2f(0, 0);
-	// 	createInfo.origin = Vector2f(36, 36);
-	// 	createInfo.frameSize = Vector2i(72, 72);
-	// 	createInfo.scale = Vector2f(1.0f, 1.0f);
-	// 	createInfo.depth = 0;
-	// 	createInfo.zoomModifier = 1.f;
+	{
+		SpriteCreateInfo createInfo = {};
+		createInfo.name = "Menu";
+		createInfo.tex = textures["Menu 0"];
+		createInfo.alpha = 255;
+		createInfo.flip = SDL_FLIP_NONE;
+		createInfo.pos = Vector2f(0, 0);
+		createInfo.origin = Vector2f(36, 36);
+		createInfo.frameSize = Vector2i(72, 72);
+		createInfo.scale = Vector2f(1.0f, 1.0f);
+		createInfo.depth = 0;
+		createInfo.zoomModifier = 1.f;
 
-	// 	// createInfo.doesAnimate = true;
-	// 	// createInfo.animationSet = loadAnimationFile("res/title.json");
+		// createInfo.doesAnimate = true;
+		// createInfo.animationSet = loadAnimationFile("res/title.json");
 
-	// 	world.createMenu(createInfo, 10);	
-	// }
+		world.createMenu(createInfo, 10);	
+	}
 }
 
 // Should the game quit?
